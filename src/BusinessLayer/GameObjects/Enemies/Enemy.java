fd63f7f7d8362e12e47e8a0e397eb9a83f811a46
Batel Shkolnik <batelshk@post.bgu.ac.il>
2021-05-22 16:41:04 +0300

    Made the range method part of the abstract class

--- src/BusinessLayer/GameObjects/Enemies/Enemy.java
+++ src/BusinessLayer/GameObjects/Enemies/Enemy.java
@@ -1,7 +1,12 @@
 package BusinessLayer.GameObjects.Enemies;
 
+import BusinessLayer.GameObjects.Player.Player;
 import Game_Tiles.Unit;
 
 public abstract class Enemy extends Unit {
     Integer experienceValue;
+
+    public double range(Enemy enemy, Player player){
+        return Math.sqrt(Math.pow((enemy.xPos-player.xPos),2)+Math.pow((enemy.yPos-player.yPos),2));
+    }
 }
