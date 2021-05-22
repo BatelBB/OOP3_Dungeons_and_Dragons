c04417d23a232107727e42f3a0518d0ce3605979
Batel Shkolnik <batelshk@post.bgu.ac.il>
2021-05-22 12:15:28 +0300

    Implemented the constructor and updateState and attack and range methods

--- src/BusinessLayer/GameObjects/Enemies/Trap.java
+++ src/BusinessLayer/GameObjects/Enemies/Trap.java
@@ -1,12 +1,44 @@
 package BusinessLayer.GameObjects.Enemies;
 
-public class Trap implements Enemy{
-    public Integer visibilityTime;
-    public Integer invisibilityTime;
-    public Integer ticksCount = 0;
-    public boolean visible = true;
+import BusinessLayer.GameObjects.Player.Player;
+
+public class Trap extends Enemy{
+    public Integer visibilityTime; // amount of ticks that the trap remains visible, a constructor argument
+    public Integer invisibilityTime; //amount of ticks that the trap remains invisible, a constructor argument.
+    public Integer ticksCount; //counts the number of ticks since last visibility state change. Initially 0.
+    public boolean visible; //indicates whether the trap is currently visible. Initially true
 
     public Trap(String name, char tile, int health, int attack, int defense, int experienceValue, int visibilityTime, int invisibilityTime){
+        this.name = name;
+        this.tile = tile;
+        this.healthAmount = health;
+        this.healthPool = health;
+        this.attackPoints = attack;
+        this.defensePoints = defense;
+        this.visibilityTime = visibilityTime;
+        this.invisibilityTime = invisibilityTime;
+        this.experienceValue = experienceValue;
+        this.ticksCount = 0;
+        this.visible = true;
+    }
+
+    public void updateState(Player player){
+        visible = ticksCount < visibilityTime;
+        if(ticksCount == (visibilityTime + invisibilityTime))
+            ticksCount = 0;
+        else
+            ticksCount +=1;
+        if(range(this, player)<2)
+            attack(player);
+    }
+
+    private void attack(Player player){
 
     }
+
+    private double range(Trap trap, Player player){
+        return Math.sqrt(Math.pow((trap.xPos-player.xPos),2)+Math.pow((trap.yPos-player.yPos),2));
+    }
+
+
 }
