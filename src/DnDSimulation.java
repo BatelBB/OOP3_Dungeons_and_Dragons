50ac571286e0213349233aa9d42d7c3a0578f2e7
Batel Shkolnik <batelshk@post.bgu.ac.il>
2021-05-22 16:40:45 +0300

    Changed three Monster objects to Boss object

--- src/DnDSimulation.java
+++ src/DnDSimulation.java
@@ -1,3 +1,4 @@
+import BusinessLayer.GameObjects.Enemies.Boss;
 import BusinessLayer.GameObjects.Enemies.Monster;
 import BusinessLayer.GameObjects.Enemies.Trap;
 import BusinessLayer.GameObjects.Player.Hunter;
@@ -23,9 +24,9 @@ public class DnDSimulation {
         Monster bearWright = new Monster("Bear-Wright", 'b', 1000, 75, 30, 4, 250);
         Monster giantWright = new Monster("Giant-Wright",'g', 1500, 100, 40, 5, 500);
         Monster whiteWalker = new Monster("White Walker", 'w', 2000, 150, 50, 6, 1000);
-        Monster theMountain = new Monster("The Mountain", 'M', 1000, 60, 25, 6, 500);
-        Monster queenCersei = new Monster("Queen Cersei",'C', 100, 10, 10, 1,1000);
-        Monster nightsKing = new Monster("Night's King", 'K', 5000, 300, 150, 8, 5000);
+        Boss theMountain = new Boss("The Mountain", 'M', 1000, 60, 25, 6, 500);
+        Boss queenCersei = new Boss("Queen Cersei",'C', 100, 10, 10, 1,1000);
+        Boss nightsKing = new Boss("Night's King", 'K', 5000, 300, 150, 8, 5000);
 
         Trap bonusTrap = new Trap("Bonus Trap", 'B', 1, 1, 1, 250, 1, 5);
         Trap queensTrap =new Trap("Queen's Trap", 'Q', 250, 50, 10, 100, 3, 7);
