af03738efad793a785c7455d27ca8fe966a4f9f7
Batel Shkolnik <batelshk@post.bgu.ac.il>
2021-05-22 12:10:37 +0300

    Changed to extends Enemy

--- src/BusinessLayer/GameObjects/Enemies/Boss.java
+++ src/BusinessLayer/GameObjects/Enemies/Boss.java
@@ -2,7 +2,8 @@ package BusinessLayer.GameObjects.Enemies;
 
 import BusinessLayer.GameObjects.Player.HeroicUnit;
 
-public class Boss implements Enemy, HeroicUnit {
+
+public class Boss extends Enemy implements HeroicUnit {
     public Integer visionRange;
     public Integer abilityFrequency;
     public Integer combatTicks;
--- src/BusinessLayer/GameObjects/Enemies/Monster.java
+++ src/BusinessLayer/GameObjects/Enemies/Monster.java
@@ -1,6 +1,7 @@
 package BusinessLayer.GameObjects.Enemies;
 
-public class Monster implements Enemy{
+
+public class Monster extends Enemy{
     public Integer visionRange;
 
     public Monster(String name, char tile, int health, int attack, int defense, int visionRange, int experienceValue){
