c327c74081f4fc9f5e9dc4abfcac9aa03ae3803e
Batel Shkolnik <batelshk@post.bgu.ac.il>
2021-05-22 12:11:49 +0300

    Changed to extends tile and added the visitor methods

--- src/Game_Tiles/Empty.java
+++ src/Game_Tiles/Empty.java
@@ -1,4 +1,23 @@
 package Game_Tiles;
 
-public class Empty {
+public class Empty extends Tile{
+    @Override
+    public void accept(Visitor visitor) {
+
+    }
+
+    @Override
+    public void visit(Wall wall) {
+
+    }
+
+    @Override
+    public void visit(Unit unit) {
+
+    }
+
+    @Override
+    public void visit(Empty empty) {
+
+    }
 }
